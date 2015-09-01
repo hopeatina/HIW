1d806dd4d419468b46a32196f655ed44eca9c649
hopeatina <hopeatina@gmail.com>
2015-09-01 16:53:18 -0500

    requirejs almost setup completely

---
+++ public/javascripts/home.js
@@ -0,0 +1,33 @@
+﻿define(['myapp'], function(myapp) {
+    myapp.factory("UserService", function () {
+        var users = ["Peter", "Daniel", "Nina"];
+        console.log(users);
+        //var locales = [];
+        return {
+            all: function () {
+                return users;
+            },
+            first: function () {
+                return users[0];
+            },
+            last: function () {
+
+            }
+        };
+    });
+
+
+    myapp.controller("MyCtrl", function ($scope, UserService) {
+        $scope.users = UserService.all();
+        //$scope.locales = UserService.hiw();
+    });
+
+    myapp.controller("YourCtrl", function ($scope, UserService) {
+        $scope.users = UserService.first();
+        //$scope.locales = UserService.hiw();
+    });
+
+    myapp.controller("AnotherCtrl", function ($scope, UserService) {
+        $scope.firstUser = UserService.first();
+    });
+})
